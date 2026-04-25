f044d66abe8c9fdf4ce519d9d17eb53f2aa1892f
Gabriel Rosa <62349836+bagriel01@users.noreply.github.com>
2026-04-25 12:32:38 -0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,7 +2,6 @@ import pytest
 # from playwright.sync_api import sync_playwright
 
 @pytest.fixture
-
 # def page():
 #     with sync_playwright() as p:
 #         browser = p.chromium.launch(headless=False)  # Set headless=True for headless mode
